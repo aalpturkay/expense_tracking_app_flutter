240cd6d4fe35382158bd259cd11c2fed5755830e
Aykut Alp Turkay <aykutalpturkay@gmail.com>
2022-02-07 17:44:00 +0300

    Revert "Update path_constants.dart"
    This reverts commit d9e064cf842cf79c422ee015e17c84bfee8fbf9a.

--- lib/constants/path_constants.dart
+++ lib/constants/path_constants.dart
@@ -1,5 +1,5 @@
 class PathConstants {
-  static String get baseURL => "your base url here";
+  static String get baseURL => "https://alp-expense-tracking-app.herokuapp.com";
   static String get registerUrl => "$baseURL/auth/signup";
   static String get loginUrl => "$baseURL/auth/signin";
   static String get expenseUrl => "$baseURL/expense";
